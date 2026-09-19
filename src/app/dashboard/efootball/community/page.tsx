06b709217cc8f223b3a7e91a4f79dd7c33fea492
Zihan231 <zihanislam231@gmail.com>
2026-09-19 15:29:18 +0600

    fix: /dashboard/efootball/community

--- src/app/dashboard/efootball/community/page.tsx
+++ src/app/dashboard/efootball/community/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 import Link from "next/link";
 import { useLanguage } from "@/lib/i18n/LanguageContext";
 import { useSession } from "@/lib/session/SessionContext";
